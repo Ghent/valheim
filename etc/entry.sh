9958cd4d991a03bd5ba8300f639e777c69f0d383
Ghent <ghentlinux@gmail.com>
2021-02-07 23:08:10 -0500

    Minor changes

--- etc/entry.sh
+++ etc/entry.sh
@@ -3,13 +3,13 @@ export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 
 mkdir -p "${STEAMAPPDIR}" || true
+cd "${STEAMAPPDIR}"
 
 bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
                 +force_install_dir "${STEAMAPPDIR}" \
                 +app_update "${STEAMAPPID}" \
                 +quit
 
-cd "${STEAMAPPDIR}"
 
-# NOTE: Minimum password length is 5 characters & Password cant be in the server name.
+# NOTE: Minimum password length is 5 characters & password can't be in the server name
 ./valheim_server.x86_64 -name "${SERVER_NAME}" -port ${SERVER_PORT} -world "${SERVER_WORLD}" -password "${SERVER_PASSWORD}" -public ${SERVER_PUB}
